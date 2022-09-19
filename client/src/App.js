5f47f59a3ff86d4593fdb777291eb812fa16dc8c
Breanne <BreMarotta@gmail.com>
2022-09-19 13:44:45 -0400

    basic routing set up. background put in for default

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect }from 'react';
+import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navigation from './features/Navigation';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -10,9 +12,11 @@ const App = () => {
     .then(data => setCount(data.count));
   }, [])
 
+
   return (
     <BrowserRouter>
       <div className="App">
+        <Navigation />
         <Switch>
           <Route path="/testing">
             <h1>Test Route</h1>
---
+++ client/src/features/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+
+  return (
+    <div className="navigation" >
+        <h1 style={{color: "linen"}}>Coffee Shop</h1>
+        <NavLink to='/' className="navLink">Home</NavLink>
+        <NavLink to='/testing' className="navLink">Testing Route</NavLink>
+        <NavLink to='/announcements' className="navLink">Announcements</NavLink>
+        <NavLink to='/drinks' className="navLink">Drink Menu</NavLink>
+        <NavLink to='/food' className="navLink">Food Menu</NavLink>
+        <NavLink to='/seasonal' className="navLink">Specials</NavLink>
+
+        <br/>
+        <hr/>
+        <br/>
+    </div>
+  )
+}
+
+export default Navigation
