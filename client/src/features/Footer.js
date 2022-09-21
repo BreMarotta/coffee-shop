4fa9d89cde38d2ce8308309125fe24d2d97568a1
Breanne <BreMarotta@gmail.com>
2022-09-21 13:35:10 -0400

    move admin login to navigation and fix styling

--- client/src/features/Footer.js
+++ client/src/features/Footer.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+
 
 const Footer = () => {
   return (
     <div className="footer">
-        <NavLink to='/login'>Admin Login</NavLink>
-        <br/>
         <>Site Creator Information: </>
         <a href="https://bremarotta.wixsite.com/breanne"target="_blank">Portfolio</a>
         <a href="https://www.linkedin.com/in/bremarotta/" target="_blank">LinkedIn</a>
-        <a href="https://github.com/BreMarotta/maintenace-tracker" target="_blank">Github</a>
+        <a href="https://github.com/BreMarotta/coffee-shop" target="_blank">Github</a>
 
     </div>
   )
--- client/src/features/Navigation.js
+++ client/src/features/Navigation.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 
+
 const Navigation = () => {
 
   return (
     <div className="navigation" >
-        <h1 style={{color: "bisque"}}>Coffee Shop</h1>
+      <NavLink to='/login' className="adminLink">Admin Login</NavLink>
+      <br/>
+      <h1 style={{color: "bisque"}}>Coffee Shop</h1>
         <NavLink to='/' className="navLink">Home</NavLink>
         <NavLink to='/announcements' className="navLink">Announcements</NavLink>
         <NavLink to='/drinks' className="navLink">Drink Menu</NavLink>
         <NavLink to='/food' className="navLink">Food Menu</NavLink>
         <NavLink to='/gallery' className="navLink">Image Gallery</NavLink>
-
-        <br/>
-        <hr/>
+        <hr />
         <br/>
     </div>
   )
