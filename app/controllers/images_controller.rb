540d29967effd06a4afef9c628873359cd65fa3b
Breanne <BreMarotta@gmail.com>
2022-09-24 18:12:59 -0400

    set up ImagesForm, including route and controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,14 @@
-class ImagessController < ApplicationController 
+class ImagesController < ApplicationController 
+
+    skip_before_action :authorize, only: :index
+
+    def index
+        render json: Image.all, status: :ok
+    end
 
     def create 
-    
+        new_image = Image.create!(image_params)
+        render json: new_image, status: :created
     end
 
     def update
@@ -14,8 +21,8 @@ class ImagessController < ApplicationController
 
     private
     
-    def announcement_params
-        params.require(:image).permit()
+    def image_params
+        params.require(:image).permit(:title, :img, :active)
     end
 
 end
--- client/src/features/Images/ImageForm.js
+++ client/src/features/Images/ImageForm.js
@@ -6,9 +6,45 @@ const ImageForm = () => {
         active: true,
         img: ""
     })
-  return (
-    <div>ImageForm</div>
-  )
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log(imageObj)
+      fetch('/images', {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(imageObj)
+      })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
+  const handleChange = (e) => {
+      const newObj = {...imageObj, [e.target.name]: e.target.value};
+      setImageObj(newObj)
+  }
+
+  const toggleActive = (e) => {
+      const newObj = {...imageObj, ["active"]: !imageObj.active}
+      setImageObj(newObj)
+  }
+
+return (
+  <form onSubmit={handleSubmit}>
+      <h1>Add Announcement</h1>
+      <hr/>
+      <label>Title: </label>
+          <input type="text" name="title" value={imageObj.title} onChange={handleChange}/>
+          <br/><br/>
+      <label>Image: </label>
+          <input type="text" name="img" value={imageObj.img} onChange={handleChange}/>
+          <br/><br/>
+      <label>Active ? </label>
+          <input type="checkbox" checked={imageObj.active} onChange={toggleActive}/>
+          <br/><br/>
+      <button type="submit">Save</button>
+      <br/><br/>
+  </form>
+)
 }
 
 export default ImageForm
